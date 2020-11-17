c97c0c3099325ba0a172a9ec292c975a2b6f1ccb
joseph tidwell <joseph.tidwell09@gmail.com>
2020-11-17 11:27:54 -0600

    .gitignore file added

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UrbanPlanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
